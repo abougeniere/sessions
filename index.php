2a28a6117157ba27a8dcbe28a814117cab75e96f
abougeniere <abougeniere@yahoo.fr>
2015-03-17 22:07:14 +0100

    1

---
+++ index.php
@@ -0,0 +1,5 @@
+<html>
+<?php
+    header('Location: login.php');
+    exit;
+?>
---
+++ testb.php
@@ -0,0 +1,112 @@
+/* global bootbox */
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+        <title>Bootstrap 101 Template</title>
+
+        <!-- Bootstrap 
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        -->
+        <link href="css/bootstrap.css" rel="stylesheet">
+
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+
+        <!-- Latest compiled and minified CSS 
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        -->
+        <link rel="stylesheet" href="css/bootstrap.css">
+
+        <!-- Optional theme 
+        <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+        -->
+        <link rel="stylesheet" href="css/bootstrap-theme.css">
+
+        <!-- Latest compiled and minified JavaScript 
+        <script src="js/bootstrap.min.js"></script>
+        -->
+        <script src="js/bootstrap.js"></script>
+        
+         <!-- bootbox code -->
+        <script src="js/bootbox.min.js"></script> 
+        
+    </head>
+    
+    <body>
+        
+        
+        <!-- test base -->
+        <br><br>
+        <div class="container">
+            <div class="row clearfix">
+                <div class="col-md-2 column">
+                </div>
+                <div class="col-md-8 column">
+                    <div class="row clearfix">
+                        <div class="col-md-12 column">
+                            <div class="jumbotron well">
+                                <h1>
+                                    Login
+                                </h1>
+                                <p>
+                                    texte login..
+                                </p>
+                                <p>
+                                     <button onclick="bootbox.alert('Hello world!');">Click me</button> 
+                                    <a class="btn btn-primary btn-large" href="#">Learn more</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <form role="form">
+                        <div class="form-group">
+                            <label for="inputEmail">Email address</label><input class="form-control" id="InputEmail" type="email" placeholder="Type your email" required/>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="InputPassword">Password</label><input class="form-control" id="InputPassword" type="password" placeholder="Type your password" required/>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="captcha">Captcha</label>
+                        
+                            <img src="includes/coolcaptcha/captcha.php"  alt="140x140" id="captcha" class="img-thumbnail img-responsive center-block" />                        
+                        </div>
+
+                        <div class="form-group">
+                            <input class="form-control" id="saisieCaptcha" type="text" placeholder="Type the captcha text" required/>
+                        </div>
+
+
+                        <button type="submit" class="btn btn-default">Submit</button>
+                    </form>
+                </div>
+                <div class="col-md-2 column">
+                </div>
+            </div>
+        </div>
+        
+
+            
+
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="js/bootstrap.min.js"></script>
+                 <!-- bootbox code -->
+        <script src="js/bootbox.min.js"></script> 
+        
+        <!-- test bootstrap -->
+        
+    </body>
+</html>
+
